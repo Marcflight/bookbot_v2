106a10e7f65609e27cd21c35710530f1e37ca286
marcflight <markus.lovenjak@web.de>
2025-03-02 21:25:25 +0100

    add num_of_chars function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from stats import get_num_of_words
+from stats import get_num_of_words, get_num_of_chars
 
 def main():
-    filepath = "books/frankenstein.txt"
-    text = get_book_text(filepath)
-    num_words = get_num_of_words(text)
+    filepath = "books/frankenstein.txt"                     #define File
+    text = get_book_text(filepath)                          #extract file as a str
+    num_words = get_num_of_words(text)                      #count words in file str
+    num_of_char = get_num_of_chars(text)                    #count characters in file str 
     print(f"{num_words} words found in the document")
+    print(num_of_char)
 
-def get_book_text(path):
+def get_book_text(path):                                    #open file and extract text as str
     with open(path) as f:
         return f.read()
     
--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
 def get_num_of_words(text):
-    words = text.split()
-    word_count = len(words)
-    return word_count
+    words = text.split()                            #split text str into words
+    word_count = len(words)                         #count number of words
+    return word_count                               #return count
+
+def get_num_of_chars(text):                         
+    num_of_chars = {}                               #create chars dict
+    for char in text:                               #loop over all chars in text string
+        lowered = char.lower()                      #convert char into lower case
+        if lowered in num_of_chars:                 #check if char already in num_of_chars dict
+            num_of_chars[lowered] += 1              #add 1 to value of lowered char
+        else:
+            num_of_chars[lowered] = 1               #else create new key and set value to one
+    return num_of_chars                             #return dictionary
