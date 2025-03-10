5961b11dbbb23580908eb15a4af49e064e017ebf
marcflight <markus.lovenjak@web.de>
2025-03-10 14:45:29 +0100

    Adding multiple book processing

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import sys
 from stats import get_num_of_words, get_num_of_chars, sort_num_of_chars
 
 def main():
-    filepath = "books/frankenstein.txt"                     #define File
+    filepath = extract_file(sys.argv)                                   #define File
     text = get_book_text(filepath)                          #extract file as a str
     num_words = get_num_of_words(text)                      #count words in file str
     num_of_chars = get_num_of_chars(text)                   #count characters in file str
@@ -11,6 +12,12 @@ def main():
 def get_book_text(path):                                    #open file and extract text as str
     with open(path) as f:
         return f.read()
+    
+def extract_file(path):
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return sys.argv[1]
     
 def print_report(filepath, num_words, sorted_chars):
     print("============ BOOKBOT ============")
